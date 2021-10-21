ab3d131639d13b3c1d17c5d8301e4d1f05047e67
Caplimbo <zhhqu0305@gmail.com>
2021-10-21 15:28:06 +0800

    processes directly on GPU

--- coref/coref_model.py
+++ coref/coref_model.py
@@ -223,7 +223,7 @@ class CorefModel:  # pylint: disable=too-many-instance-attributes
         full_result = []
         for doc, bert_doc in zip(docs, bert_docs):
             # print(f"bert doc shape:", bert_doc.shape)
-            words, attn_duration = self.we(doc, bert_doc.to(self.config.device))
+            words, attn_duration = self.we(doc, bert_doc)
 
             # Obtain bilinear scores and leave only top-k antecedents for each word
             # top_rough_scores  [n_words, n_ants]
@@ -373,7 +373,7 @@ class CorefModel:  # pylint: disable=too-many-instance-attributes
         # Obtain bert output for selected batches only
         # attention_mask = (subwords_batches != self.tokenizer.pad_token_id)
 
-        full_output = torch.tensor([])
+        full_output = torch.tensor([], device=self.config.device)
         bert_time = 0
         for index in range(0, len(subwords_batches), bert_batch_size):
             subwords_batches_tensor = torch.tensor(subwords_batches[index: index+bert_batch_size], device=self.config.device, dtype=torch.long)
@@ -383,15 +383,14 @@ class CorefModel:  # pylint: disable=too-many-instance-attributes
             with torch.no_grad():
                 out = self.bert(
                     subwords_batches_tensor,
-                    attention_mask=attention_mask)['last_hidden_state'].detach().cpu()
+                    attention_mask=attention_mask)['last_hidden_state']
             # full_output = out.detach().cpu()[subword_mask_tensor[index: index+bert_batch_size].to(self.config.device)]
             bert_time += time.time() - start
             full_output = torch.cat([full_output, out])
             del out
 
-        separate_output = [full_output[split_index[i]: split_index[i+1]][subword_mask_tensor[split_index[i]: split_index[i+1]]] for i in range(len(docs))]
+        separate_output = [full_output[split_index[i]: split_index[i+1]][subword_mask_tensor[split_index[i]: split_index[i+1]].to(self.config.device)] for i in range(len(docs))]
         # separate_output = [full_output]
-        del full_output
         return separate_output, bert_time
 
     def _build_model(self):
